9022fc94f764373ea30335d96739f728ef731290
ALAMIN Dolar <you@example.com>
2021-12-04 01:52:34 +0600

    navbar example added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Al-amin Portfolio</h1>
-      <h3>Full-stack Web Developer</h3>
+      <Header></Header>
     </div>
   );
 }
