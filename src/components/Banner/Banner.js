c5ec06a1861ed7d3b581e3219a1fa43fbaee06cc
ALAMIN Dolar <you@example.com>
2021-12-05 00:42:34 +0600

    Banner ok

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './Banner.css';
+import profile from '../../images/Profile.png';
 
 const Banner = () => {
     return (
         <div>
-            <section>
+            <section className='section-display'>
                 <div className="half-width">
                     <h1>Md.Al-amin</h1>
                     <h3>Full-Stack Web Developer</h3>
                     <p> Experienced work with React.js, MongoDB, express, firebase, Heroku. Some projects already I make using my learning skill. Now a job internship is needed to shine my skill.</p>
-                    
+                    <a href="" className='btn btn-warning'><strong>Hire Me</strong></a>
                 </div>
 
                 <div className="half-width">
-                <h1>Md.Al-amin</h1>
-                    <h3>Full-Stack Web Developer</h3>
-                    <p> Experienced work with React.js, MongoDB, express, firebase, Heroku. Some projects already I make using my learning skill. Now a job internship is needed to shine my skill.</p>
+                
                     
-                  <img src="../../images/Profile.png" alt="" />
+                  <img src={profile}alt="" />
                 </div>
             </section>
         </div>
