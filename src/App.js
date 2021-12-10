060a24f106241efd0d2c3bfb257edfe3159dfd07
ALAMIN Dolar <you@example.com>
2021-12-10 22:56:38 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Banner from './components/Banner/Banner';
 import Navigation from './components/Navigation/Navigation';
 import About from './components/About/About';
 import Project from './components/Project/Project';
+import Contact from './components/Contact/Contact';
 
 
 
@@ -17,6 +18,7 @@ function App() {
       <Banner></Banner>
       <About></About>
       <Project></Project>
+      <Contact></Contact>
     
     </div>
   );
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,53 @@
+import React, { useRef } from 'react';
+import './Contact.css'
+import contactImg from '../../../images/my/about-9.jpg'
+import { Col, Row } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import emailjs from 'emailjs-com';
+
+
+const Contact = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const form = useRef();
+    // const onSubmit = data => {
+    //     console.log(data)
+    //     reset()
+    // };
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_7xjxp0e', 'template_4cwj8ci', e.target, 'user_WUImOdavvetW0VE0Cnub5')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    };
+
+    return (
+        <div className="about-container">
+            {/* <h4 className="text-warning">About</h4> */}
+            <Row>
+                <Col xs={12} md={6}>
+                    <img className="img-fluid image-about" src={contactImg} alt="" />
+                </Col>
+                <Col xs={12} md={6}>
+                    <form ref={form} onSubmit={sendEmail}>
+
+                        <input className="contact" type="text" name="name" placeholder="name" />
+
+                        <input className="contact" type="email" name="email" placeholder="email" />
+
+                        <input className="contact" type="text" name="subject" placeholder="subject" />
+
+                        <textarea className="contact" name="message" placeholder="write massage" />
+                        <input className="button" type="submit" value="Send" />
+                    </form>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default Contact;
