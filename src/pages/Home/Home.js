4114e361053e6c97d31831cd75f5aa544d62569f
ALAMIN Dolar <you@example.com>
2021-12-15 05:06:57 +0600

    Fake data added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
 import Navigation from '../Header/Navigation';
+import Projects from '../Projects/Projects';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
            <Navigation></Navigation> 
            <Banner></Banner>
            <About></About>
+           
         </div>
     );
 };
